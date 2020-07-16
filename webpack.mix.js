7f042bc8cc0a01d6a194969376dc91bd2b382990
Tyler Jefford <tylerjefford@gmail.com>
2020-07-15 21:49:29 -0500

    add purgeCSS for tailwind optimization

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,6 @@
 const mix = require('laravel-mix');
 const tailwindcss = require('tailwindcss');
+const purgeCss = require('laravel-mix-purgecss');
 
 /*
  |—————————————————————————————————————
@@ -12,9 +13,13 @@ const tailwindcss = require('tailwindcss');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/styles.pcss', 'public/css/styles.css')
+mix.sass('resources/sass/styles.pcss', 'public/css/styles.css')
     .options({
         processCssUrls: false,
         postCss: [tailwindcss('./tailwind.config.js')],
+    })
+    .purgeCss({
+      enabled: mix.inProduction(),
+      folders: ['resources/views'],
+      extensions: ['php'],
     });
